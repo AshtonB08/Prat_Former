779e7bcfdd07fb7d3a73f3c4d285744cd724d95f
AshtonB08 <93924688+AshtonB08@users.noreply.github.com>
2024-09-25 12:55:36 +0100

    added basics of jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D playerRb;
     public float moveSpeed;
+    public float jumpForce;
+
+    private Vector2 movement;
+
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.AddForce(new Vector2(playerRb.velocity.x, jumpForce));
+        }
     }
+
+	private void FixedUpdate()
+	{
+		playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.deltaTime);
+	}
 }
