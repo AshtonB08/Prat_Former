8e8d40db127835179693320f85d7c7e85690d233
AshtonB08 <93924688+AshtonB08@users.noreply.github.com>
2024-09-25 12:35:17 +0100

    basic player movement beginning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Rigidbody2D playerRb;
+    public float moveSpeed;
     // Update is called once per frame
     void Update()
     {
