c83c94acf5b6a08ece0d25d601f539eb5f7ff791
AshtonB08 <93924688+AshtonB08@users.noreply.github.com>
2024-10-17 17:47:41 +0100

    restarted playerController. player will only move left and right.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
+  
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float moveSpeed = 10f;
 
-    private Vector2 movement;
-
-    [SerializeField]
-    private Rigidbody2D player;
-
-    //called once upon game start
-	private void Awake()
+	private void Start()
 	{
-		player = GetComponent<Rigidbody2D>();   //initalises players rigid body on game start
-
+		rb = GetComponent<Rigidbody2D>();   //initalises players rigid body 
 	}
 
 	// FixedUpdate is called once per frame at a fixed rate
 	void FixedUpdate()
     {
-       player.velocity = new Vector2 (Input.GetAxis("Horizontal") * moveSpeed, player.velocity.y);  //player left/right movement logic
-
-        /* PLAYER JUMP LOGIC */
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            player.velocity = new Vector2(player.velocity.x, moveSpeed);    //player maintains speed when jumping
-        }
+		//player movement logic
+		moveInput = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2 (moveInput * moveSpeed, rb.velocity.y);  
+  
     }
 
 
